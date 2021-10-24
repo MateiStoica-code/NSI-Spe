66c403e501b4ee8837ecfcaebfcab74343f9419d
Matei Stoica <72706773+MateiStoica-code@users.noreply.github.com>
2021-10-24 11:04:31 +0200

    Update exercice 2 : pour bien démarrer.py

--- exercice 2 : pour bien démarrer.py
+++ exercice 2 : pour bien démarrer.py
@@ -2,8 +2,21 @@ from random import *
 
 L= [randint(1,100) for i in range(20)]
 print(L)
+
+
 def sum_list(list) :
     somme=0
     for i in range(len(list)) :
         somme += list[i]
     return(somme)
+
+
+def min_max(list) :
+    mini = min(list)
+    maxi = max(list)
+    return(mini, maxi)
+
+def amplitude(list) :
+    values = min_max(list)
+    ampli = values[1] - values[0]
+    return(ampli)
